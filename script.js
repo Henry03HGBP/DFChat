adb33fe4b32695ce8ea3d033f098f35ea796b6e8
Henry03HGBP <henry.bautista.prado@icloud.com>
2026-01-20 11:26:12 -0600

    agregando funciones con javascript

---
+++ script.js
@@ -0,0 +1,11 @@
+function whatssapp() {
+    location.href = "https://wa.me/5217713643825"
+}
+
+function callphone() {
+    location.href = "tel:+527713643825"
+}
+
+function meet() {
+    location.href = "https://meet.google.com/xxx-yyyy-zzz"
+}
