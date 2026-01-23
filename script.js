a57989e8d996f5e9ed0d8094fdd101fe16d348e8
Henry03HGBP <henry.bautista.prado@icloud.com>
2026-01-23 13:13:29 -0600

    Agregue números para brindar soporte tecnico

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function whatssapp() {
+function Darki() {
     location.href = "https://wa.me/5217713643825"
 }
 
-function callphone() {
-    location.href = "tel:+527713643825"
+function Ivan() {
+    location.href = "https://wa.me/5217711987163"
+}
+
+function Demetrio() {
+    location.href = "https://wa.me/5217717787659"
+}
+
+function Henry() {
+    location.href = "https://wa.me/5217711165216"
 }
 
 function meet() {
